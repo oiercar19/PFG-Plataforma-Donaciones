7b12c67497db1793c0364f6ce2e8d79bca0accbc
Oier Carbón <oier.carbon@opendeusto.es>
2026-02-11 00:05:03 +0100

    fix (Frontend): remove invisible character

--- frontend/src/pages/admin/AdminPanel.js
+++ frontend/src/pages/admin/AdminPanel.js
@@ -1,4 +1,4 @@
-﻿import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { adminAPI } from '../../services/api';
 import './AdminPanel.css';
 
